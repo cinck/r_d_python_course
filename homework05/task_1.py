cf419432d2689d823ed378d92ab6b563e5afeaaf
cinck <cinckforit@gmail.com>
2023-03-15 22:43:27 +0200

    Single print for all conditions

--- homework05/task_1.py
+++ homework05/task_1.py
@@ -6,20 +6,30 @@ while user_input.lower() != "exit":
     print("----------------------------------------")
 
     for char in user_input:
+        char_type = ""
+        char_property = ""
+
         if char.isdigit():
-            odd_or_even = "odd"
+            char_type = "number"
+            char_property = "an odd"
             if int(char) % 2 == 0:
-                odd_or_even = "even"
-            print(f"'{char}' is {odd_or_even} number")
+                char_property = "an even"
+
         elif char.isalpha():
-            case = "upper case"
+            char_type = "letter"
+            char_property = "an upper case"
             if char.islower():
-                case = "lower case"
-            print(f"'{char}' is {case} letter")
+                char_property = "a lower case"
+
         elif char.isspace():
-            print(f"'{char}' is a space")
+            char_type = "space"
+            char_property = "a"
+
         else:
-            print(f"'{char}' is a symbol")
+            char_type = "symbol"
+            char_property = "a"
+
+        print(f"'{char}' is {char_property} {char_type}")
 
     print("----------------------------------------")
     user_input = input("Please enter your data or type 'exit' to finish: -> ")
