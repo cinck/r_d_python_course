132ab77e2e069da9d03113367b2cbd35ac178b39
cinck <cinckforit@gmail.com>
2023-03-21 20:20:02 +0200

    Comments added

--- homework06/task_01.py
+++ homework06/task_01.py
@@ -1,8 +1,14 @@
-def rise_to_power(number, power):
+# Program that raises numbers to power
+# User can entry number and power values for calculation.
+# Note only natural numbers are accepted for both numbers and powers.
+
+# HW.Task 1: Function that raises number to power from two arguments.
+def raise_to_power(number, power):
     return number ** power
 
 
-def users_entry(entry_request: str):
+# Accepts user's input, checks it and returns integer if entry data is correct.
+def users_entry(entry_request: str) -> int:
     number_input = input(f"Enter {entry_request}: -> ")
     if number_input and number_input.isdigit():
         return int(number_input)
@@ -11,13 +17,16 @@ def users_entry(entry_request: str):
         return users_entry(entry_request)
 
 
+# Main function
 def run():
+    print(" -= RAISING TO POWER =-")
     number = users_entry("your number")
     power = users_entry("power value")
     print("---------RESULT---------")
-    print(f"{number}^{power} = {rise_to_power(number, power)}")
+    print(f"{number}^{power} = {raise_to_power(number, power)}")
     print("------------------------")
-    if input("Type 'R' to retry or anything else to finish: -> ").lower() == "r":
+    if input("Type 'R' to restart or anything else to finish: -> ").lower() == "r":
+        print("------------------------")
         run()
 
 
