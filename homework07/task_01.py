1b484e9b2676e47ddf0b8ecf37ba73d5800d7343
cinck <cinckforit@gmail.com>
2023-03-23 18:07:19 +0200

    ADD, CHECK NAME functions done

--- homework07/task_01.py
+++ homework07/task_01.py
@@ -7,8 +7,38 @@ def stats():
     pass
 
 
+def check_available_name(name, names):
+    if not name or name in names:
+        if not name:
+            print("Entered nothing.")
+        else:
+            print("Contact already exists.")
+        print("Record NOT crated")
+        return False
+    return True
+
+
 # add record
-def add():
+def add(phonebook):
+    contact_data = {"Phone number": "",
+                    "E-mail": "",
+                    "Address": ""
+                    }
+    print("Enter contact name")
+    name = input("Name: -> ")
+
+    if check_available_name(name, phonebook.keys()):
+        print(f"Enter {name} contact data")
+        for data in contact_data.keys():
+            contact_data[data] = input(f"{data}: > ")
+        phonebook[name] = contact_data
+        print(f"Contact '{name}' created.")
+        return phonebook
+
+    return phonebook
+
+
+
     pass
 
 
@@ -27,20 +57,41 @@ def show_name(name):
     pass
 
 
-# take command
-def execute_command(command, record_book):
+def show_help():
     pass
 
 
-def main():
-    phonebook = {}
+# take command
+def execute_command(command: str, phonebook: dict) -> dict:
+    executable = command.split()
+    match executable[0].lower():
+        case "stats":
+            pass
+        case "add":
+            pass
+        case "delete":
+            pass
+        case "list":
+            pass
+        case "show":
+            pass
+        case "help":
+            pass
+        case _:
+            print("Invalid command. Try 'help' for info.")
+            return phonebook
+    return dict()
+
+
+def main(phonebook={}):
     command = None
     while command != "exit":
         command = input("@-> ")
         if not command or command == "exit":
             continue
         else:
-            pass
+            phonebook = execute_command(command, phonebook)
+
     pass
 
 
