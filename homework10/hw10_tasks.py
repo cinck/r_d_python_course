1194166b93a92cb4f5cceb92f7b6a44a0ae746bd
cinck <cinckforit@gmail.com>
2023-04-04 21:15:10 +0300

    factorial func + comments

--- homework10/hw10_tasks.py
+++ homework10/hw10_tasks.py
@@ -1,9 +1,13 @@
-from myfrequentfuncs.myfuncs import intentry
-
-# 1. Написати рекурсію, яка буде друкувати числа від введенного користувачем до нуля.
+from myfrequentfuncs.myfuncs import intentry  # function to accept only integers from input()
 
 
+# <HW10> Task_1. Recursive function that prints countdown from entered number
 def backward_count(number: int):
+    """
+    Prints reversed count from number
+    :param number: any integer
+    :return: None
+    """
     print(number, end=" ")
     if number == 0:
         print()
@@ -11,21 +15,39 @@ def backward_count(number: int):
     backward_count(number - 1)
 
 
+# <HW10> Task_2*. Recursive function to get fibonacci numbers
 def fibonacci(number: int):
+    """
+    Returns value of n element in Fibonacci sequence
+    :param number: int, sequence number of element
+    :return: int
+    """
     if number <= 0:
         return 0
     elif number == 1:
         return 1
     else:
-        return fibonacci(number-1) + fibonacci(number-2)
+        return fibonacci(number - 1) + fibonacci(number - 2)
 
+
+# <WH10> Task_3*. Recursive function to calculate factorial
+def factorial(number: int):
+    """
+    Returns calculated value of argument in factorial
+    :param number: int
+    :return: int
+    """
+    if number <= 0:
+        return 1
+    return number * factorial(number - 1)
 
 
 if __name__ == "__main__":
     some_number = intentry(rangemax=995)
+    print("-------------------------")
     print(f"Backward count from {some_number}")
     backward_count(some_number)
-    for i in range(some_number):
-        print(f"Fibonacci n{i} is {fibonacci(i)}")
+    print("-------------------------")
     print(f"Fibonacci n{some_number} is {fibonacci(some_number)}")
-
+    print("-------------------------")
+    print(f"Factorial calculation: {some_number}! = {factorial(some_number)}")
