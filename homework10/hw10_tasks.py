9303df8c4ae57a9b4b1bf5a48bb90a2a7cc3e778
cinck <cinckforit@gmail.com>
2023-04-04 20:09:51 +0300

    backwardcounter + fibonacci

--- homework10/hw10_tasks.py
+++ homework10/hw10_tasks.py
@@ -3,16 +3,29 @@ from myfrequentfuncs.myfuncs import intentry
 # 1. Написати рекурсію, яка буде друкувати числа від введенного користувачем до нуля.
 
 
-def backward_count(number):
+def backward_count(number: int):
     print(number, end=" ")
     if number == 0:
         print()
-        return None
-    return backward_count(number - 1)
+        return
+    backward_count(number - 1)
+
+
+def fibonacci(number: int):
+    if number <= 0:
+        return 0
+    elif number == 1:
+        return 1
+    else:
+        return fibonacci(number-1) + fibonacci(number-2)
+
 
 
 if __name__ == "__main__":
     some_number = intentry(rangemax=995)
+    print(f"Backward count from {some_number}")
     backward_count(some_number)
-
+    for i in range(some_number):
+        print(f"Fibonacci n{i} is {fibonacci(i)}")
+    print(f"Fibonacci n{some_number} is {fibonacci(some_number)}")
 
