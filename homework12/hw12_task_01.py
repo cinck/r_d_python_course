870f2bad8269b980ba3e3cb213d1e4fce4b2ffa6
cinck <cinckforit@gmail.com>
2023-04-19 18:33:56 +0300

    some items renamed

--- homework12/hw12_task_01.py
+++ homework12/hw12_task_01.py
@@ -1,23 +1,23 @@
-from time import strftime as displaytime, localtime, sleep
+from time import strftime as s_time, localtime, sleep
 
 
 # <HW12> Task1
-def func_name_start(some_func):
+def func_name_time(some_func):
     """Decorator function displays name of function which is decorated and start time of function operation"""
 
     def inner_func():
-        print(f"Function '{some_func.__name__}' started at {displaytime('%H:%M:%S', localtime())}")
+        print(f"Function '{some_func.__name__}' started at {s_time('%H:%M:%S', localtime())}")
         some_func()
 
     return inner_func
 
 
-@func_name_start
+@func_name_time
 def my_function():
     print("This is my function")
 
 
-@func_name_start
+@func_name_time
 def my_function2():
     print("My second function")
 
--- homework12/hw12_task_03.py
+++ homework12/hw12_task_03.py
@@ -1,9 +1,9 @@
-from myfrequentfuncs.myfuncs import intentry
-from time import strftime as displaytime, localtime
+from myfrequentfuncs.myfuncs import intentry  # function to obtain integers from input
+from time import strftime as s_time, localtime
 
 
 # <HW12> Task 3
-def name_start_deco(times=0):
+def name_time_deco(times=0):
     """Decorator function can display name and start time of decorated function multiple times
     if 'times' argument is not 0
     :param times: quantity of printed name+time lines
@@ -14,7 +14,7 @@ def name_start_deco(times=0):
 
         def inner_func():
             for i in range(times):
-                print(f"{i+1}) Function '{some_func.__name__}' started at {displaytime('%H:%M:%S', localtime())}")
+                print(f"{i+1}) Function '{some_func.__name__}' started at {s_time('%H:%M:%S', localtime())}")
             some_func()
 
         return inner_func
@@ -22,7 +22,7 @@ def name_start_deco(times=0):
     return wrap
 
 
-@name_start_deco(times=intentry())
+@name_time_deco(times=intentry())
 def print_func():
     print("This is the FUNCTION")
 
