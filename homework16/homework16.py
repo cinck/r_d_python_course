ba021f50a163b1aa4c91b49b1f7be625054aee05
cinck <cinckforit@gmail.com>
2023-05-03 22:19:26 +0300

    multiprocessing with Pool done

--- homework16/homework16.py
+++ homework16/homework16.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import threading as thr
+import multiprocessing as mp
 
 # 1. За допомогою https://open-meteo.com/ дістати прогноз погоди для 5ти ваших улюблених міст на планеті.
 #    Реалізувати за допомогою модуля requests з використанням мультипотоковості і мультипроцесорності.
@@ -12,29 +13,28 @@ import threading as thr
 
 class City:
 
-    def __init__(self, name: str, latitude: float, longitude: float):
+    def __init__(self, name: str, latitude: float, longitude: float, days=1):
         self.name = name
         self.coordinates = {"latitude": latitude, "longitude": longitude}
         self.latitude = latitude
         self.longitude = longitude
-        self.temp_forcast = None
+        self.temp_forecast = None
         self.average_temp = None
-        self.forcast_days = None
+        self.forecast_days = days
 #        self.get_temp_forcast(1)
 
-    def get_temp_forcast(self, days: int, attempt=0):
+    def get_temp_forcast(self, days=1, attempt=0):
         try:
             resp = requests.get(
                 url="https://api.open-meteo.com/v1/forecast",
                 params={
                     "latitude": self.latitude,
                     "longitude": self.longitude,
-                    "forecast_days": days,
+                    "forecast_days": self.forecast_days,
                     "hourly": "temperature_2m",
                 }
             )
-            self.temp_forcast = resp.json()["hourly"]["temperature_2m"]
-            self.forcast_days = days
+            self.temp_forecast = resp.json()["hourly"]["temperature_2m"]
 
         except Exception as ex:
             print(ex)
@@ -46,35 +46,60 @@ class City:
             self.set_avg_temp()
 
     def set_avg_temp(self):
-        if self.temp_forcast:
-            self.average_temp = sum(self.temp_forcast)/len(self.temp_forcast)
+        if self.temp_forecast:
+            self.average_temp = round(sum(self.temp_forecast) / len(self.temp_forecast), 2)
+
+
+def get_forecast(city):
+    city.get_temp_forcast()
+    return city
+
+
+def execute_in_processes(locations: dict):
+    mp_start = time.time()
+    cities2 = []
+
+    for city_name, coordinates in locations.items():
+        cities2.append(City(city_name, *coordinates))
+
+    with mp.Pool(5) as pool:
+        forecasted = pool.map(get_forecast, cities2)
+
+    print(f"Multiprocessing time: {time.time() - mp_start}")
+
+    return forecasted
 
 
 def execute_in_threading(locations: dict):
-    cities = {}
+    mt_start = time.time()
+    cities = []
     threads = []
-    thr_no = 0
+    counter = 0
     for city, coordinates in locations.items():
-        cities[city] = City(city, *coordinates)
-        threads.append(thr.Thread(target=cities[city].get_temp_forcast, args=(2, )))
-        threads[thr_no].start()
-        print(f"Cycle: {thr_no}\nActive threads: {thr.active_count()}")
-        thr_no += 1
+        cities.append(City(city, *coordinates))
+        threads.append(thr.Thread(target=cities[counter].get_temp_forcast, args=(1, )))
+        threads[counter].start()
+        # print(f"Cycle: {counter}\nActive threads: {thr.active_count()}")
+        counter += 1
 
     for thread in threads:
         thread.join()
 
+    print(f"Threading time: {time.time() - mt_start}")
+
     return cities
 
 
-def get_hottest_city(cities: dict):
+def get_hottest_city(cities: list):
     max_temp_city = None
-    for city in cities.values():
-        print(f"{city.name} temp= {city.average_temp}C")
+
+    for city in cities:
         if not max_temp_city:
             max_temp_city = city
         elif city.average_temp > max_temp_city.average_temp:
             max_temp_city = city
+
+    print(f"Hottest city is {max_temp_city.name} with average temperature {max_temp_city.average_temp} C")
 
     return max_temp_city
 
@@ -90,10 +115,11 @@ if __name__ == "__main__":
         "Sydney": (-33.87, 151.21),
     }
 
-    cities = execute_in_threading(locations)
+    mt_cities = execute_in_threading(locations)
 
-    hottest_city = get_hottest_city(cities)
+    mp_cities = execute_in_processes(locations)
 
-    print(f"Hottest city is {hottest_city.name} with average temperature {hottest_city.average_temp}")
+    get_hottest_city(mt_cities)
+    get_hottest_city(mp_cities)
 
-    print(time.time()-t_start)
+    print(f"Full time {time.time()-t_start}")
