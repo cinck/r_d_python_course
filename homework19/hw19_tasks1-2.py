7aade8732d9492594ec1c5d06b4e6dfbe2833ae9
cinck <cinckforit@gmail.com>
2023-05-11 19:23:14 +0300

    Tasks 1,2 comments

--- homework19/hw19_tasks1-2.py
+++ homework19/hw19_tasks1-2.py
@@ -1,25 +1,55 @@
 class Bot:
+    """
+    <HW19> Task1. Bot class.
+    """
     def __init__(self, name):
         self.name = name
 
     def say_name(self):
+        """
+        Prints 'name' attribute
+        :return:
+        """
         print(f"Hi! My name is {self.name}!")
 
     def send_message(self, message: str):
+        """
+        Prints 'message' argument
+        :param message: str()
+        :return:
+        """
         print(message)
 
 
 class TelegramBot(Bot):
+    """
+    <HW19> Task2. Telegram bot class.
+    """
     url = None
     chat_id = None
 
     def set_url(self, url: str):
+        """
+        Sets url ref to attribute
+        :param url: str()
+        :return:
+        """
         self.url = url
 
     def set_id(self, chat_id: str):
+        """
+        Sets chad_id attribute.
+        :param chat_id: str()
+        :return:
+        """
         self.chat_id = chat_id
 
     def send_message(self, message: str):
+        """
+        Prints message with additional comments
+        :param message: str()
+        :return:
+        """
         print(
             f"{self.name} speaking in {self.chat_id} group and says:\n{message}\n"
             f"Message sent via {self.url}"
