5e03c7b2e5cff280b8e05a7641617c3c7c52279e
cinck <cinckforit@gmail.com>
2023-06-18 00:43:52 +0300

    tasks 1 - 4 done

---
+++ homework28/hw28.js
@@ -0,0 +1,53 @@
+const cntnrDiv = document.createElement("div")
+cntnrDiv.className = "container"
+
+const bttnsDiv = document.createElement("div");
+bttnsDiv.className = "container cntnr-bttns"
+bttnsDiv.idName = "buttons";
+bttnsDiv.textAlign = "center";
+
+btnNames = ["Add friend", "Send message", "Offer a job"]
+btnNames.map(buttonName => {
+    let button = document.createElement("button");
+    button.className = "btn btn-primary";
+    button.innerText = buttonName;
+    button.style.margin = "3px";
+    bttnsDiv.appendChild(button);
+})
+
+let userInfo = document.getElementById("userInfo");
+userInfo.appendChild(bttnsDiv);
+
+let passTaskBttn = document.createElement("button");
+passTaskBttn.className = "btn btn-success";
+passTaskBttn.innerText = "Pass task";
+
+const bttnsDiv2 = document.createElement("div")
+bttnsDiv2.className = "container cntnr-bttns"
+bttnsDiv2.id = "add-task"
+bttnsDiv2.style.paddingLeft = "5px"
+bttnsDiv2.appendChild(passTaskBttn)
+document.getElementById("studyProgress").appendChild(bttnsDiv2).appendChild(passTaskBttn);
+
+// let frndsQty = Math.round(Math.random() * 200).toString();
+document.getElementById("frnds-qty").innerText = Math.round(Math.random() * 200).toString();
+
+const addFriendBttn = document.getElementsByTagName("button")[0];
+addFriendBttn.addEventListener("click", (buttonClickEvent) => {
+    buttonClickEvent.target.style.backgroundColor = "grey";
+    buttonClickEvent.target.innerText = "Waiting confirmation";
+    buttonClickEvent.target.disabled = true;
+});
+
+function changeColour(bttnColour) {
+    if (bttnColour === "green") {return "pink"}
+    else {return "green"}
+}
+
+const sendMessageBttn = document.getElementsByTagName("button")[1];
+sendMessageBttn.style.backgroundColor = "green";
+
+sendMessageBttn.addEventListener("click", (bttnClick) => {
+    bttnClick.target.style.backgroundColor = changeColour(sendMessageBttn.style.backgroundColor);
+});
+
