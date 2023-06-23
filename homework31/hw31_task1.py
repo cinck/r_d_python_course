f48962932a51f9447e027361e45bbcbb1724f558
cinck <cinckforit@gmail.com>
2023-06-23 15:35:55 +0300

    output responces done

--- homework31/hw31_task1.py
+++ homework31/hw31_task1.py
@@ -1,5 +1,6 @@
 import requests
 import random
+import json
 
 
 def random_url():
@@ -7,12 +8,32 @@ def random_url():
         "https://google.com",
         "https://facebook.com",
         "https://twitter.com",
-        "https://amazon.com",
+        "https://amazon.com",  # never responds for some reason
         "https://apple.com"
     ]
     return random.choice(urls)
 
 
+def get_response_from(url: str):
+    try:
+        result = requests.get(url=url)
+    except Exception:
+        return None
+    return result
+
+
+def show_response_from(url: str):
+    print(url)
+    response = get_response_from(url)
+    if not response:
+        print("Failed to connect")
+    else:
+        html_len = len(response.text)
+        print(response)
+        print(f"HTML code lengths: {html_len} characters")
+
+
 if __name__ == "__main__":
     for _ in range(10):
-        print(random_url())
+        show_response_from(random_url())
+        print("---------------------------------------------")
