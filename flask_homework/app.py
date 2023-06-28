0062ed7b09da41e5ec1ae5892fc35dbfea64fd74
cinck <cinckforit@gmail.com>
2023-06-28 22:24:37 +0300

    logging added

--- flask_homework/app.py
+++ flask_homework/app.py
@@ -1,10 +1,30 @@
 from flask import Flask
+from logging.config import dictConfig
+
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def root_page():
+    app.logger.info('Request to endpoint "/"')
     return '''
     <div><a href='http://127.0.0.1:5000/hello'>Hello page</a></div>
     <div>-</div>
@@ -16,11 +36,13 @@ def root_page():
 
 @app.route('/hello')
 def hello_world():
-    return 'Hello World!'
+    app.logger.info('Request to endpoint "/hello"')
+    return '<h1>Hello World!</h1>'
 
 
 @app.route('/json')
 def return_json():
+    app.logger.info('Request to endpoint "/json"')
     return [
         {'item 1': {'parameter 1': 'value 1', 'parameter 2': 'value 2'}},
         {'item 2': {'nested item': {'parameter 1': 'value 1', 'parameter 2': 'value 2'}}}
@@ -29,8 +51,9 @@ def return_json():
 
 @app.route('/html')
 def return_html():
+    app.logger.info('Request to endpoint "/html"')
     return '''
-    <h1>Return HTML</h1>
+    <h1>This is HTML return</h1>
     '''
 
 
