3a181b14f5eef9827847c65cffbb1bb618b8cc0e
cinck <cinckforit@gmail.com>
2023-06-28 22:02:10 +0300

    hw tasks 1-3 done

--- flask_homework/app.py
+++ flask_homework/app.py
@@ -4,9 +4,35 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def root_page():
+    return '''
+    <div><a href='http://127.0.0.1:5000/hello'>Hello page</a></div>
+    <div>-</div>
+    <div><a href='http://127.0.0.1:5000/json'>Return json</a></div>
+    <div>-</div>
+    <div><a href='http://127.0.0.1:5000/html'>Return html</a></div>
+    '''
+
+
+@app.route('/hello')
+def hello_world():
     return 'Hello World!'
 
 
+@app.route('/json')
+def return_json():
+    return [
+        {'item 1': {'parameter 1': 'value 1', 'parameter 2': 'value 2'}},
+        {'item 2': {'nested item': {'parameter 1': 'value 1', 'parameter 2': 'value 2'}}}
+    ]
+
+
+@app.route('/html')
+def return_html():
+    return '''
+    <h1>Return HTML</h1>
+    '''
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
