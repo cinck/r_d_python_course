d64e1796f1556deabc4d27bc6937c58ab907ae19
cinck <cinckforit@gmail.com>
2023-07-05 00:59:26 +0300

    404, 500 response changed

--- flask_homework/ehandlers.py
+++ flask_homework/ehandlers.py
@@ -4,9 +4,17 @@ from app import app
 
 @app.errorhandler(r_errors.NotFound)
 def handle_not_found(e):
-    return "Can't find it anywhere... Are you sure you need it?", 404
+    response = """
+        <h1>Bad luck! Sorry!</h1>
+        <h3>Can't find it anywhere... Are you sure you need it?</h3>
+    """
+    return response, 404
 
 
 @app.errorhandler(r_errors.InternalServerError)
 def handle_internal_server_error(e):
-    return "Oh, shit! That shouldn't be like that. Sorry!", 500
+    response = """
+        <h1>Oh, shit! Me apologize!</h1>
+        <h3>That shouldn't be like that.</h3>
+    """
+    return response, 500
