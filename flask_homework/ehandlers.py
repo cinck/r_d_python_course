3f056e6f81e0fd73614129ad359893b206c10011
cinck <cinckforit@gmail.com>
2023-07-05 17:35:09 +0300

    comments added

--- flask_homework/ehandlers.py
+++ flask_homework/ehandlers.py
@@ -1,9 +1,16 @@
 import werkzeug.exceptions as r_errors
 from app import app
 
+# <HW33> Task 5. Custom error handlers for errors 404, 500.
+
 
 @app.errorhandler(r_errors.NotFound)
 def handle_not_found(error):
+    """
+    Error 404 handler
+    :param error:
+    :return:
+    """
     response = f"""
         <h1>Bad luck! Sorry! Can't find it anywhere... </h1>
         <h3>{error}. Are you sure you need it?</h3>
@@ -13,6 +20,11 @@ def handle_not_found(error):
 
 @app.errorhandler(r_errors.InternalServerError)
 def handle_internal_server_error(error):
+    """
+    Error 500 handler
+    :param error:
+    :return:
+    """
     response = f"""
         <h1>Oh, shit! Me apologize!</h1>
         <h3>{error}. That shouldn't be like that.</h3>
