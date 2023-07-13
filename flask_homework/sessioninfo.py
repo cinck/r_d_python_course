6b2a298935099836d1f58c478607e208b41418a8
cinck <cinckforit@gmail.com>
2023-07-14 00:19:57 +0300

    unused lines deleted

--- flask_homework/sessioninfo.py
+++ flask_homework/sessioninfo.py
@@ -1,8 +1,5 @@
-from config import AppConfigData
 from flask import redirect, session
 from app import app
-
-app.secret_key = AppConfigData.SECRET_KEY
 
 
 # <HW34> Task 2. Adding username to session
