a318a9d5d532688af1bd8f6983e6aa3d81941d5f
cinck <cinckforit@gmail.com>
2023-07-17 14:32:54 +0300

    User model

--- djangoProject/user/models.py
+++ djangoProject/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.IntegerField(unique=True, primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.PositiveSmallIntegerField(null=False)
+
+    class Meta:
+        db_table = 'users'
+
