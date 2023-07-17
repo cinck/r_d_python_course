3a06afd78b8293410a0f6fe3eba3ff4fb5a3274c
cinck <cinckforit@gmail.com>
2023-07-17 13:54:15 +0300

    new apps references

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user.apps.UserConfig',
+    'book.apps.BookConfig',
+    'purchase.apps.PurchaseConfig',
 ]
 
 MIDDLEWARE = [
