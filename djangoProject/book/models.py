97a24815640ec94dc37a0e7f06380c88a0d8b564
cinck <cinckforit@gmail.com>
2023-07-17 14:33:06 +0300

    book model

--- djangoProject/book/models.py
+++ djangoProject/book/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    id = models.IntegerField(unique=True, primary_key=True)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=100)
+    year = models.PositiveSmallIntegerField(null=False)
+    price = models.FloatField(null=False)
+
+    class Meta:
+        db_table = 'Books'
