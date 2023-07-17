89a6a145d743dd37cc0771415e00c1d8d9474a6f
cinck <cinckforit@gmail.com>
2023-07-17 15:18:02 +0300

    remaned table

--- djangoProject/book/models.py
+++ djangoProject/book/models.py
@@ -10,5 +10,5 @@ class Book(models.Model):
     price = models.FloatField(null=False)
 
     class Meta:
-        db_table = 'Books'
+        db_table = 'books'
         unique_together = ['title', 'author']   # <HW37> Task 5. Unique title-author combination
