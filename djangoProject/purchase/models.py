d4c79aea88f5270307aa5f32948282828f00edd5
cinck <cinckforit@gmail.com>
2023-07-17 14:41:57 +0300

    purchase model updated

--- djangoProject/purchase/models.py
+++ djangoProject/purchase/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 class Purchase(models.Model):
     id = models.IntegerField(unique=True, primary_key=True)
+    date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey('User', related_name='purchases', on_delete=models.CASCADE())
     book = models.ForeignKey('Book', related_name='books', on_delete=models.CASCADE())
+
 
     class Meta:
         db_table = 'purchases'
