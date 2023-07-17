f5009a730a7fcc37267bbcb1ad25eaa8431634e0
cinck <cinckforit@gmail.com>
2023-07-17 15:17:26 +0300

    purchase model import relations

--- djangoProject/purchase/models.py
+++ djangoProject/purchase/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from user.models import User
+from book.models import Book
 
 
 # <HW> Task 2. Create models
 class Purchase(models.Model):
     id = models.IntegerField(unique=True, primary_key=True)
     date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey('User', related_name='purchases', on_delete=models.CASCADE)
-    book = models.ForeignKey('Book', related_name='books', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='purchases', on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, related_name='books', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'purchases'
-        ordering = '-date'          # <HW37> Task 5. Sorting in ascending order
+        ordering = ['-date']          # <HW37> Task 5. Sorting in ascending order
