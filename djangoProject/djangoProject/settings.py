f22d3e6ff6f53a87add23a8808c26b8b57b999e1
cinck <cinckforit@gmail.com>
2023-07-29 20:34:07 +0300

    project settings.py: project-based DRF pagination

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -136,4 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
 # DRF config
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
+}
