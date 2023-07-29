57a95f6e8dfc958ad65ba739ba223877699a762a
cinck <cinckforit@gmail.com>
2023-07-29 20:47:35 +0300

    user/api: custom pagination page_size=10

--- djangoProject/users/api/views.py
+++ djangoProject/users/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 
+from users.api.pagination import UserPagination
 from users.api.serializers import UserSerializer
 from users.models import User
 
@@ -7,3 +8,4 @@ from users.models import User
 class UserModelViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    pagination_class = UserPagination
