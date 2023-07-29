b6f6d42c7e8a9c6e102b587951da8ad4147387ef
cinck <cinckforit@gmail.com>
2023-07-29 20:55:37 +0300

    django-filter package installed

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'users.apps.UsersConfig',
     'book.apps.BookConfig',
     'purchase.apps.PurchaseConfig',
