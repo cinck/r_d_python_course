03aa31594b32dd21d4f0f862cf06110b1c1f3741
cinck <cinckforit@gmail.com>
2023-07-29 16:01:22 +0300

    users/url.py: router added

--- djangoProject/users/urls.py
+++ djangoProject/users/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from users.api import views as api_views
 from users import views
 
 app_name = 'users'
+
+router = routers.SimpleRouter()
+router.register('', api_views.UserModelViewSet)
 
 urlpatterns = [
     path('', views.UsersListView.as_view(), name='users-list'),
     path('<int:pk>', views.UsersDetailView.as_view(), name='users-detail'),
     path('create/', views.UsersCreateView.as_view(), name='users-create'),
+    path('api/', include(router.urls))
 ]
