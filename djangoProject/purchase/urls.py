72e73d0e193129b72cff9cfd239f0178205cfcd5
cinck <cinckforit@gmail.com>
2023-07-29 19:57:21 +0300

    purchases/urls.py: DRF router

--- djangoProject/purchase/urls.py
+++ djangoProject/purchase/urls.py
@@ -1,11 +1,17 @@
 
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from purchase.api import views as api_views
 from purchase import views
 
 app_name = 'purchase'
+
+router = routers.SimpleRouter()
+router.register('', api_views.PurchaseModelViewSet)
 
 urlpatterns = [
     path('', views.PurchaseListView.as_view(), name='purchase-list'),
     path('<int:pk>', views.PurchaseDetailView.as_view(), name='purchase-detail'),
     path('create/', views.PurchaseCreateView.as_view(), name='purchase-create'),
+    path('api/', include(router.urls))
 ]
