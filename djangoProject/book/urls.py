8274802007d23372228791bfe14d17789bdcbcdc
cinck <cinckforit@gmail.com>
2023-07-29 16:01:35 +0300

    book/urls.py: router added

--- djangoProject/book/urls.py
+++ djangoProject/book/urls.py
@@ -1,17 +1,19 @@
 
 from django.urls import path, include
 from book import views
+from book.api import views as api_views
 from rest_framework import routers
 
 
 app_name = 'book'
 
-# router = routers.SimpleRouter()
-# router.register('', views.BookViewSet)
+router = routers.SimpleRouter()
+router.register('', api_views.BookModelViewSet)
 
 urlpatterns = [
     path('', views.BookListView.as_view(), name='books-list'),
     path('<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('create/', views.BookCreateView.as_view(), name='book-create'),
-    # path('', include(router.urls))
+    path('api/', include(router.urls))
 ]
+
