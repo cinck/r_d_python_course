e56e8f1285214fac175a410aebb3c3b3bd51baac
cinck <cinckforit@gmail.com>
2023-07-30 15:28:25 +0300

    /purchase: custom filter, ViewSet updated

--- djangoProject/purchase/api/views.py
+++ djangoProject/purchase/api/views.py
@@ -1,5 +1,8 @@
+import django_filters.rest_framework as df
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
 
+from purchase.api.filters import PurchaseFilter
 from purchase.api.serializers import PurchaseSerializer
 from purchase.models import Purchase
 
@@ -7,3 +10,10 @@ from purchase.models import Purchase
 class PurchaseModelViewSet(viewsets.ModelViewSet):
     queryset = Purchase.objects.all()
     serializer_class = PurchaseSerializer
+
+    # default_filter_backend = df.DjangoFilterBackend
+    filter_backends = [df.DjangoFilterBackend, SearchFilter, OrderingFilter]
+
+    filterset_class = PurchaseFilter
+    ordering_fields = ['date']
+    ordering = ['date']
