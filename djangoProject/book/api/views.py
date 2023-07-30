8fc173473ea4d6a9bd0b7844c16f66d3a029d220
cinck <cinckforit@gmail.com>
2023-07-30 14:59:06 +0300

    /book: custom filter, ViewSet updated

--- djangoProject/book/api/views.py
+++ djangoProject/book/api/views.py
@@ -1,9 +1,19 @@
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
+import django_filters.rest_framework as df
 
 from book.api.serializers import BookSerializer
 from book.models import Book
+from book.api.filters import BookFilter
 
 
 class BookModelViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    default_filter_backend = df.DjangoFilterBackend
+    filter_backends = [default_filter_backend, SearchFilter, OrderingFilter]
+    filterset_class = BookFilter
+    search_fields = ['title', 'author']
+    ordering_fields = ['author', 'title']
+    ordering = ['author']
