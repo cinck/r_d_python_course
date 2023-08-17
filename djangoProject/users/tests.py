c2ed3086d3c14e7893a2322c3e52bc2124e9b810
cinck <cinckforit@gmail.com>
2023-08-17 21:15:50 +0300

    hw45 tassk2: test User model create

--- djangoProject/users/tests.py
+++ djangoProject/users/tests.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestUserModel(TestCase):
+    test_data = {
+        'username': 'test_name',
+        'password': 'test_password',
+        'age': '18'
+    }
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create(username='test_name', password='test_password', age=18)
+
+    def test_user_creation(self):
+        self.assertEqual(self.user.username, self.test_data['username'])
+        self.assertEqual(self.user.password, self.test_data['password'])
+        self.assertEqual(self.user.age, self.test_data['age'])
