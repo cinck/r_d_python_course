539a0a4a811d389c254fd9f2f5ad529beadb5f10
cinck <cinckforit@gmail.com>
2023-08-18 22:03:25 +0300

    user/tests.py: tests done

--- djangoProject/users/tests.py
+++ djangoProject/users/tests.py
@@ -17,3 +17,45 @@ class TestUserModel(TestCase):
         self.assertEqual(self.user.username, self.test_data['username'])
         self.assertEqual(self.user.password, self.test_data['password'])
         self.assertEqual(self.user.age, self.test_data['age'])
+
+
+class TestUserViews(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(username='test_user', password='test_password', age='18')
+
+    def test_users_list_view(self):
+        response = self.client.get('/users/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/users_list.html')
+        self.assertContains(response, 'test_user')
+
+    def test_users_detail_view(self):
+        response = self.client.get(f'/users/{self.user.pk}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/users_detail.html')
+        self.assertContains(response, 'test_user')
+
+    def test_users_create_view(self):
+        response = self.client.post(
+            f'/users/create/',
+            {
+                'username': 'test_user_create',
+                'password': 'test_password',
+                'age': 18
+            }
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_users_delete_view(self):
+        response = self.client.post(
+            f'/users/create/',
+            {
+                'username': 'test_user_create',
+                'password': 'test_password',
+                'age': 18
+            }
+        )
+        self.assertEqual(response.status_code, 302)
+
